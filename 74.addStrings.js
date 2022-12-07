9a4b8c321c3a0a162d7cd4e713b714b997f06c4f
ygcongHh <ygconghh@foxmail.com>
2022-12-07 14:10:03 +0800

    update

--- 74.addStrings.js
+++ 74.addStrings.js
@@ -13,10 +13,10 @@ var addStrings = function (num1, num2) {
     const x = i >= 0 ? num1.charAt(i) - '0' : 0;
     const y = j >= 0 ? num2.charAt(j) - '0' : 0;
     const result = x + y + add;
-    ans.push(result % 10);
+    ans.unshift(result % 10);
     add = Math.floor(result / 10);
     i -= 1;
     j -= 1;
   }
-  return ans.reverse().join('');
+  return ans.join('');
 };
